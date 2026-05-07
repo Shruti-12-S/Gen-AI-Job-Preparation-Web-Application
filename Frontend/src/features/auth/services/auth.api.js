ab7d09f0d01ba7ddc19e62b0609d1bc7ec7697d9
Shruti-12-S <shruti21.shinde@gmail.com>
2026-05-07 12:39:48 +0530

    fix production backend url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-job-preparation-web-application-cpvl.onrender.com",
     withCredentials: true
 })
 
