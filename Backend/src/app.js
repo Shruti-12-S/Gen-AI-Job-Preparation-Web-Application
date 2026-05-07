ca94a6ea7478f4dc0f8dcf44a02989816bf5c9f2
Shruti-12-S <shruti21.shinde@gmail.com>
2026-05-07 12:31:17 +0530

    fix cors and root route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,18 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://gen-ai-job-preparation-web-application-1-a9wi.onrender.com"
+    ],
     credentials: true
-}))
+}));
+
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully");
+});
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
